5ab1a875a3d9f2c2f5a1f6612099cbf1ffe461af
CaptCalvin <72896558+Captcalvin07@users.noreply.github.com>
2025-04-03 01:56:50 -0400

    Update SilksongGenerator.py
    Created generate_status function and percentage chance

--- SilksongGenerator.py
+++ SilksongGenerator.py
@@ -1,3 +1,9 @@
 from datetime import datetime
-day_of_year = datetime.now().timetuple().tm_yday
-print(str(365-day_of_year) + " days remain")
+
+def generate_status():
+  day_of_year = datetime.now().timetuple().tm_yday
+  days_remaining = 365-day_of_year
+  percentage_chance = "{:.4f}".format(100/days_remaining)
+  return ("{percentage_chance}% chance Silksong tommorow. {days_remaining} days remain in 2025")
+
+print(generate_status())
