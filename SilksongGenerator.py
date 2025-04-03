e8863e6c74b2bef0f63af20493f30fdc0db18ba9
CaptCalvin <72896558+Captcalvin07@users.noreply.github.com>
2025-04-03 01:59:08 -0400

    Update SilksongGenerator.py
    made days_remaining a str

--- SilksongGenerator.py
+++ SilksongGenerator.py
@@ -4,6 +4,6 @@ def generate_status():
   day_of_year = datetime.now().timetuple().tm_yday
   days_remaining = 365-day_of_year
   percentage_chance = "{:.4f}".format(100/days_remaining)
-  return percentage_chance + "% chance Silksong tommorow. " + days_remaining + " days remain in 2025"
+  return percentage_chance + "% chance Silksong tommorow. " + str(days_remaining) + " days remain in 2025"
 
 print(generate_status())
