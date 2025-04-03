49a17a6a7fa6466b853162678a07802447b00043
CaptCalvin <72896558+Captcalvin07@users.noreply.github.com>
2025-04-03 02:57:22 -0400

    Update SilksongGenerator.py
    Removed print statement.

--- SilksongGenerator.py
+++ SilksongGenerator.py
@@ -5,5 +5,3 @@ def generate_status():
   days_remaining = 365-day_of_year
   percentage_chance = "{:.4f}".format(100/days_remaining)
   return percentage_chance + "% chance Silksong tommorow. " + str(days_remaining) + " days remain in 2025"
-
-print(generate_status())
