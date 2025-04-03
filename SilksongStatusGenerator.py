f4f8249f90a7e0d0a4c23533e6b82db7b9267c90
CaptCalvin <72896558+Captcalvin07@users.noreply.github.com>
2025-04-03 17:33:45 -0400

    Update SilksongStatusGenerator.py
    ob1 error

--- SilksongStatusGenerator.py
+++ SilksongStatusGenerator.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 def generate_status():
   day_of_year = datetime.now().timetuple().tm_yday
-  days_remaining = 365-day_of_year
+  days_remaining = 365-day_of_year-1
   percentage_chance = "{:.4f}".format(100/days_remaining)
   return percentage_chance + "% chance Silksong tommorow. " + str(days_remaining) + " days remain in 2025"
