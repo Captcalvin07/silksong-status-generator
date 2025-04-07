e83edda98edddbe0a717679f732510af4db0f6f3
CaptCalvin <72896558+Captcalvin07@users.noreply.github.com>
2025-04-07 16:07:17 -0400

    Update SilksongStatusGenerator.py
    Fixed probablity on last day of month

--- SilksongStatusGenerator.py
+++ SilksongStatusGenerator.py
@@ -3,14 +3,19 @@ import calendar
 
 def generate_status():
   day_of_year = datetime.now().timetuple().tm_yday
-  current_month = datetime.now().strftime("%B")
+  current_month = (datetime.now() + timedelta(days=1)).strftime("%B")
   days_remaining = 365-day_of_year-1
   percentage_chance = "{:.4f}".format(100/days_remaining)
   chance_difference = "{:.4f}".format((100/days_remaining)-(100/(days_remaining+1)))
 
   today = date.today()
-  last_day = calendar.monthrange(today.year, today.month)[1]
-  days_remaining_month = last_day - today.day
+  tommorow = today + timedelta(days=1)
+  last_day = calendar.monthrange(tommorow.year, tommorow.month)[1]
+
+  if (tommorow.day - today.day) != 1:
+    days_remaining_month = last_day - tommorow.day + 1
+  else:
+    days_remaining_month = last_day - today.day
 
   month_chance = "{:.4f}".format(100/(days_remaining/days_remaining_month))
 
